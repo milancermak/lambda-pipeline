a603adc9c9564c22ee311292337ab4b9d2b60bf8
Milan Cermak <milan.cermak@gmail.com>
2017-04-18 15:12:49 +0200

    More descriptive var name

--- infrastructure/bootstrap.sh
+++ infrastructure/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-STACK_NAME="lambda-pipeline"
+PIPELINE_STACK_NAME="my-lambda-project-pipeline"
 TEMPLATE_URL="file://pipeline.yml"
 
-aws cloudformation create-stack --stack-name $STACK_NAME --template-body $TEMPLATE_URL --capabilities CAPABILITY_IAM
-aws cloudformation wait stack-create-complete --stack-name $STACK_NAME
-aws cloudformation describe-stacks --stack-name $STACK_NAME
+aws cloudformation create-stack --stack-name $PIPELINE_STACK_NAME --template-body $TEMPLATE_URL --capabilities CAPABILITY_IAM
+aws cloudformation wait stack-create-complete --stack-name $PIPELINE_STACK_NAME
+aws cloudformation describe-stacks --stack-name $PIPELINE_STACK_NAME
